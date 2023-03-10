0bedd35f2d6245c952f03db3983095e75f4109e8
Sergii <sergii.khylko@outlook.com>
2023-03-10 12:03:39 +0200

    #6 add css modules

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -1,35 +1,11 @@
-// import "./Button.css";
+import styles from "./Button.module.css";
 
-// const Button = (props) => {
-//   return (
-//     <button type={props.type} className="button">
-//       {props.children}
-//     </button>
-//   );
-// };
-
-import styled from "styled-components";
-
-const Button = styled.button`
-
-  font: inherit;
-  padding: 0.5rem 1.5rem;
-  color: white;
-  background: #00358b;
-  border-radius: 4px;
-  box-shadow: 0 0 4px rgba(50, 50, 50, 0.25);
-  cursor: pointer;
-  
-  
-  &:focus {
-    outline: none;
-  }
-  
-  &:hover,
-  &:active {
-    background: #245fbd;
-    box-shadow: 0 0 8px rgba(50, 50, 50, 0.25);
-  }
-  `;
+const Button = (props) => {
+  return (
+    <button type={props.type} className={styles.button}>
+      {props.children}
+    </button>
+  );
+};
 
 export default Button;
